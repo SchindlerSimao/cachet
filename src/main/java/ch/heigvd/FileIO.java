40a546e8cf2996681a8d1dc92225a1804117853b
Simão Romano Schindler <simaoschindler@gmail.com>
2025-09-27 10:18:22 +0200

    Added writeToFile

--- src/main/java/ch/heigvd/FileIO.java
+++ src/main/java/ch/heigvd/FileIO.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 public class FileIO {
     public static String fileToString(final String filePath){
-        try(InputStream inputStream = new FileInputStream(filePath)){
+        try(final InputStream inputStream = new FileInputStream(filePath)){
             return new String(inputStream.readAllBytes());
         } catch (FileNotFoundException e) {
             System.err.println("File not found: " + filePath);
@@ -12,6 +12,17 @@ public class FileIO {
             throw new RuntimeException(e);
         } catch (IOException e) {
             System.err.println("Error opening file: " + filePath);
+
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String writeToFile(final String content, final String filePath){
+        try(final OutputStream outputStream = new FileOutputStream(filePath)){
+            outputStream.write(content.getBytes());
+            return filePath;
+        } catch (IOException e) {
+            System.err.println("Error writing to file: " + filePath);
 
             throw new RuntimeException(e);
         }
