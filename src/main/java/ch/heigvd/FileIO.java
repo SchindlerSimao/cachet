eee6d6b157349d6fae5cf8358c8026e7d3678d1a
Simão Romano Schindler <simaoschindler@gmail.com>
2025-10-02 09:42:26 +0200

    Refactored FileIO.java

--- src/main/java/ch/heigvd/FileIO.java
+++ src/main/java/ch/heigvd/FileIO.java
@@ -4,15 +4,7 @@ import java.io.*;
 
 public class FileIO {
     public static String readFileString(final File filePath) {
-        try (final InputStream inputStream = new FileInputStream(filePath)) {
-            return new String(inputStream.readAllBytes());
-        } catch (FileNotFoundException e) {
-            System.err.println("File not found: " + filePath);
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            System.err.println("Error opening file: " + filePath);
-            throw new RuntimeException(e);
-        }
+        return new String(readFileRaw(filePath));
     }
 
     public static byte[] readFileRaw(final File filePath) {
@@ -37,11 +29,6 @@ public class FileIO {
     }
 
     public static void writeFileString(final String content, final File filePath) {
-        try (final OutputStream outputStream = new FileOutputStream(filePath)) {
-            outputStream.write(content.getBytes());
-        } catch (IOException e) {
-            System.err.println("Error writing to file: " + filePath);
-            throw new RuntimeException(e);
-        }
+        writeFileRaw(content.getBytes(), filePath);
     }
 }
