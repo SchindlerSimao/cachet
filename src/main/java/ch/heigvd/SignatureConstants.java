ccfcfdc370a45feac94fb56fb445f2e909dbe283
Simão Romano Schindler <simaoschindler@gmail.com>
2025-10-02 11:22:58 +0200

    Added missing comment

--- src/main/java/ch/heigvd/SignatureConstants.java
+++ src/main/java/ch/heigvd/SignatureConstants.java
@@ -1,5 +1,11 @@
 package ch.heigvd;
 
+/**
+ * This class contains constants related to signature operations.
+ */
 public final class SignatureConstants {
+    /**
+     * The signature algorithm to be used for signing and verifying data.
+     */
     public static final String SIGNATURE_ALGORITHM = "ed25519";
 }
