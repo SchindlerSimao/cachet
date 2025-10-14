feddfb10c0d95f511b93590384803f56804270d5
Simão Romano Schindler <simaoschindler@gmail.com>
2025-10-14 19:12:30 +0200

    Added missing encodings.xml, fixed misc.xml and reverted mistake from previous commit

--- src/main/java/ch/heigvd/Constants.java
+++ src/main/java/ch/heigvd/Constants.java
@@ -8,4 +8,5 @@ public final class Constants {
      * The signature algorithm to be used for signing and verifying data.
      */
     public static final String SIGNATURE_ALGORITHM = "RSASSA-PSS";
+    public static final String SIGNATURE_ALGORITHM = "Ed25519";
 }
